728977dc18fe55487737fb0de168b7c25527dfe5
Your Name <you@example.com>
2024-11-26 01:19:37 +0600

    ssl fix

--- Server/app/Services/InvoiceService.js
+++ Server/app/Services/InvoiceService.js
@@ -15,7 +15,9 @@ let ObjectId = mongoose.Types.ObjectId;
 export const CreateInvoiceService = async (req) => {
 
     let user_id = new ObjectId(req.headers.user_id._id);
+    
     let cus_email= req.headers.email
+
 
 // =============Step 01: Calculate Total Payable & Vat=====================================================================================
 
@@ -38,7 +40,6 @@ export const CreateInvoiceService = async (req) => {
 
         ]
     )
-
     let totalAmount=0;
     CardProduct.forEach((element)=>{
 
@@ -60,8 +61,12 @@ export const CreateInvoiceService = async (req) => {
 // =============Step 02: Prepare  Customer Details & Shipping Details=====================================================================================
 
     let Profile=await profileModel.aggregate([matchStage]);
+
+
+
     let cus_details=`Name:${Profile[0]['cus_name']}, Email:${cus_email}, Address:${Profile[0]['cus_add']}, Phone:${Profile[0]['cus_phone']}`;
     let ship_details=`Name:${Profile[0]['ship_name']}, City:${Profile[0]['ship_city']}, Address:${Profile[0]['ship_add']}, Phone:${Profile[0]['ship_phone']}`;
+
 
 
 // =============Step 03: Transaction & Other's ID=====================================================================================
@@ -116,6 +121,8 @@ export const CreateInvoiceService = async (req) => {
 
     let PaymentSettings=await PaymentSettingModel.find();
 
+
+
     const form=new FormData();
 
     form.append('store_id',PaymentSettings[0]['store_id']);
@@ -133,32 +140,35 @@ export const CreateInvoiceService = async (req) => {
 
     form.append('cus_name',Profile[0]['cus_name'])
     form.append('cus_email',cus_email)
-    form.append('cus_add1',Profile[0]['cus_add'])
+    form.append('cus_add1',Profile[0]['cus_add'])   
     form.append('cus_add2',Profile[0]['cus_add'])
     form.append('cus_city',Profile[0]['cus_city'])
-    form.append('cus_state',Profile[0]['cus_state'])
+    form.append('cus_state',Profile[0]['cus_State'])
     form.append('cus_postcode',Profile[0]['cus_postcode'])
     form.append('cus_country',Profile[0]['cus_country'])
     form.append('cus_phone',Profile[0]['cus_phone'])
     form.append('cus_fax',Profile[0]['cus_phone'])
+
 
     form.append('shipping_method',"YES")
     form.append('ship_name',Profile[0]['ship_name'])
     form.append('ship_add1',Profile[0]['ship_add'])
     form.append('ship_add2',Profile[0]['ship_add'])
     form.append('ship_city',Profile[0]['ship_city'])
-    form.append('ship_state',Profile[0]['ship_state'])
+    form.append('ship_state',Profile[0]['ship_State'])
     form.append('ship_country',Profile[0]['ship_country'])
     form.append('ship_postcode',Profile[0]['ship_postcode'])
+
+    
+  
 
     form.append('product_name','According Invoice')
     form.append('product_category','According Invoice')
     form.append('product_profile','According Invoice')
     form.append('product_amount','According Invoice')
 
+
     let SSLRes=await axios.post(PaymentSettings[0]['init_url'],form);
-
-
 
     return {status:"success",data:SSLRes.data}
 
